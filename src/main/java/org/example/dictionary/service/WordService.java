ab33d3a8ff1c302b2e01838c2c1bcd8206a0e154
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-06-23 13:55:11 +0300

    Add WordService with basic methods

---
+++ src/main/java/org/example/dictionary/service/WordService.java
@@ -0,0 +1,37 @@
+package org.example.dictionary.service;
+
+import lombok.Data;
+import org.example.dictionary.model.Dictionary;
+import org.example.dictionary.model.Word;
+import org.example.dictionary.repository.DictionaryRepository;
+import org.example.dictionary.repository.UserRepository;
+import org.example.dictionary.repository.WordRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+public class WordService {
+    private final UserRepository userRepository;
+    private final DictionaryRepository dictionaryRepository;
+    private final WordRepository wordRepository;
+
+    public Word addWordToDictionary(Long dictionaryId, Word word){
+        Dictionary dictionary = dictionaryRepository.findById(dictionaryId)
+                .orElseThrow(() -> new RuntimeException("Word not found"));
+
+        dictionary.addWord(word);
+
+        return wordRepository.save(word);
+    }
+
+    public void deleteWord(Word word){
+        wordRepository.delete(word);
+    }
+
+    public void deleteWordById(Long wordId){
+        Word word = wordRepository.findById(wordId)
+                .orElseThrow(() -> new RuntimeException("Word not found"));
+
+        wordRepository.delete(word);
+    }
+}
