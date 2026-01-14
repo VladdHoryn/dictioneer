502413a0ceaf71543baf1628e649d407316a2070
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-01-14 15:54:10 +0200

    Add UserDetailsServiceImpl with all logics Adapt UserRepository

---
+++ src/main/java/org/example/dictionary/config/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package org.example.dictionary.config;
+
+import org.example.dictionary.model.User;
+import org.example.dictionary.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        System.out.println("DEBUG: Пошук користувача за email: " + email);
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            System.out.println("DEBUG: Користувач не знайдений!");
+            throw new UsernameNotFoundException("User not found");
+        }
+        System.out.println("DEBUG: Користувач знайдений: " + user.getEmail());
+        System.out.println("DEBUG: Його хеш пароля: " + user.getPassword());
+        return UserDetailsImpl.build(user);
+    }
+}
